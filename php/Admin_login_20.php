48d35737d71a2e41180cdabf252922b887cf0c3d
唐明武 <544468609@qq.com>
2016-09-06 13:11:33 +0800

    管理员登录接口

--- php/Admin_login_20.php
+++ php/Admin_login_20.php
@@ -7,10 +7,10 @@
  */
 header("Content-type: text/html;charset=utf-8;");
 $array=null;
-$name=$_GET['name'];
-$password=$_GET['password'];
-$result = mysqli_query($conn,"SELECT * FROM admin WHERE name='$name'");
+$name=$_POST['name'];
+$password=$_POST['password'];
+$result = mysqli_query($conn,"SELECT * FROM admin WHERE name='$name' AND password='$password'");
 if(!(mysqli_num_rows($result))){
-    echo newjson(7,"用户名或密码错误",$array);
-}else echo newjson(8,"登陆成功",$array);
+    echo newjson(6,"用户名或密码错误",$array);
+}else echo newjson(7,"登陆成功",$array);
 ?>
