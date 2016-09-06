32c67004037bf2c1a1fc3922806c3570dd3c8d1e
唐明武 <544468609@qq.com>
2016-09-06 12:00:36 +0800

    检查token的接口

--- php/Check_token_3.php
+++ php/Check_token_3.php
@@ -7,12 +7,12 @@
  */
 header("Content-type: text/html;charset=utf-8;");
 $array=null;
-$user_ID=$_GET['user_ID'];
-$token=$_GET['token'];
+$user_ID=$_POST['user_ID'];
+$token=$_POST['token'];
 $result = mysqli_query($conn,"SELECT * FROM token WHERE token='$token' AND user_ID='$user_ID'");
 if(!(mysqli_num_rows($result))){
-    echo newjson(5,"token不存在",$array);
+    echo newjson(4,"token不存在",$array);
 }else{
-    echo newjson(6,"token存在",$array);
+    echo newjson(5,"token存在",$array);
 }
 ?>
