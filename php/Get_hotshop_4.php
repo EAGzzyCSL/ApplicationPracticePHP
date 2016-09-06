f80d2b72849c559a02f89775f89e581af1c4ef53
luongyin <2272822400@qq.com>
2016-09-06 13:53:09 +0800

    获取热门商品前20条接口

---
+++ php/Get_hotshop_4.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 红印
+ * Date: 2016/9/6
+ * Time: 11:30
+ */
+header("Content-type: text/html;charset=utf-8;");
+$array=null;
+$school_ID=$_POST['school_ID'];
+$result = mysqli_query($conn,"SELECT * FROM shop WHERE  school_ID='$school_ID' ORDER by rate DESC limit 0,20");
+if(!(mysqli_num_rows($result))){
+    echo newjson(8,"无数据",$array);
+}else {
+    $i = 0;
+    while ($row = mysqli_fetch_array($result)) {//$row = mysqli_fetch_array($result)
+        $array[$i]['ID'] = $row['ID'];
+        $array[$i]['name'] = $row['name'];
+        $array[$i]['address'] = $row['address'];
+        $array[$i]['image'] = $row['image'];
+        $array[$i]['school_ID'] = $row['school_ID'];
+        $array[$i]['rate'] = $row['rate'];
+        $i++;
+    }
+    echo newjson(9,"显示前20项成功",$array);
+}
+?>
