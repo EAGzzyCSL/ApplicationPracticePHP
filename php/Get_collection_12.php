bdef6870e7cae5c1a587d434d45661d005f5ce49
唐明武 <544468609@qq.com>
2016-09-08 20:43:21 +0800

    获取收藏接口（修改版）

--- php/Get_collection_12.php
+++ php/Get_collection_12.php
@@ -8,16 +8,21 @@
 header("Content-type: text/html;charset=utf-8;");
 $array=null;
 $user_ID=$_POST['user_ID'];
-$result = mysqli_query($conn,"SELECT * FROM collection WHERE user_ID='$user_ID'");
+$result = mysqli_query($conn,"SELECT * FROM goods where ID IN (SELECT goods_ID FROM collection where user_ID='$user_ID')");
 if(!(mysqli_num_rows($result))){
-    echo newjson(16,"账号不存在，请注册",$array);
+    echo newjson(16,"获取收藏失败",$array);
 }   else {
-    $i = 0;
-    while ($row = mysqli_fetch_array($result)) {
-        $array[$i]['goods_ID'] = $row['goods_ID'];
-        $array[$i]['user_ID'] = $row['user_ID'];
-        $i++;
+        $i = 0;
+        while ($row = mysqli_fetch_array($result)) {//$row = mysqli_fetch_array($result)
+            $array[$i]['ID'] = $row['ID'];
+            $array[$i]['name'] = $row['name'];
+            $array[$i]['price'] = $row['price'];
+            $array[$i]['shop_ID'] = $row['shop_ID'];
+            $array[$i]['school_ID'] = $row['school_ID'];
+            $array[$i]['rate'] = $row['rate'];
+            $i++;
+        }
         echo newjson(17, "获取收藏成功", $array);
-    }
+
 }
 ?>
