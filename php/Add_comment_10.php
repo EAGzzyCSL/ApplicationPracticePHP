bab210641e0df90b651904efa1c29cfcd43054e1
EAGzzyCSL <eagzzycsl@outlook.com>
2016-09-11 18:19:03 +0800

    添加上传评论和菜时插入图片的接口

--- php/Add_comment_10.php
+++ php/Add_comment_10.php
@@ -3,24 +3,30 @@
  * Created by PhpStorm.
  * User: 红印
  * Date: 2016/9/6
- * Time: 16:17
+ * Time: 16:17.
  */
-
-$array=null;
+$array = null;
 $goods_ID = $_POST['goods_ID'];
 $content = $_POST['content'];
 $rate = $_POST['rate'];
-$user_ID=$_POST['user_ID'];
-$token=$_POST['token'];
-$result = mysqli_query($conn,"SELECT * FROM token WHERE  token='$token' AND user_ID='$user_ID'");
-if(!(mysqli_num_rows($result))){
-    echo newjson(18,"token操作错误，拒绝服务",$array);
-}else{
-    $date=date('Y-m-d H:i:s',time());
+$user_ID = $_POST['user_ID'];
+$token = $_POST['token'];
+$images = $_POST['images'];
+$result = mysqli_query($conn, "SELECT * FROM token WHERE  token='$token' AND user_ID='$user_ID'");
+if (!(mysqli_num_rows($result))) {
+    echo newjson(18, 'token操作错误，拒绝服务', $array);
+} else {
+    $date = date('Y-m-d H:i:s', time());
     $sql = "INSERT INTO comment (user_ID, goods_ID, content,rate,time) VALUES ('$user_ID','$goods_ID','$content','$rate','$date')";
-    $new=mysqli_query($conn,$sql);
-    if($new) echo newjson(24,"添加评论成功",$array);
-    else echo newjson(23,"添加评论失败",$array);
+    $new = mysqli_query($conn, $sql);
+    $comment_id = $conn->insert_id;
+    $imageArray = explode(', ', substr($images, 1, -1));
+    foreach ($imageArray as $one_image) {
+        mysqli_query($conn, "INSERT INTO `comment_image`( `comment_ID`, `url`) VALUES ('$comment_id','$one_image')");
+    }
+    if ($new) {
+        echo newjson(24, '添加评论成功', $array);
+    } else {
+        echo newjson(23, '添加评论失败', $array);
+    }
 }
-
-?>
--- php/Add_goods_9.php
+++ php/Add_goods_9.php
@@ -3,22 +3,26 @@
  * Created by PhpStorm.
  * User: 红印
  * Date: 2016/9/6
- * Time: 16:17
+ * Time: 16:17.
  */
-$array=null;
+$array = null;
 $name = $_POST['name'];
 $price = $_POST['price'];
 $shop_ID = $_POST['shop_ID'];
 $school_ID = $_POST['school_ID'];
-$rate = $_POST['rate'];
-$user_ID=$_POST['user_ID'];
-$token=$_POST['token'];
-$result = mysqli_query($conn,"SELECT * FROM token WHERE  token='$token' AND user_ID='$user_ID'");
-if(!(mysqli_num_rows($result))){
-    echo newjson(18,"token操作错误，拒绝服务",$array);
-}else{
-    $sql = "INSERT INTO goods (name, price, shop_ID,school_ID,rate) VALUES ('$name','$price','$shop_ID','$school_ID','$rate')";
-    $new=mysqli_query($conn,$sql);
-    echo newjson(19,"操作成功",$array);
+$user_ID = $_POST['user_ID'];
+$token = $_POST['token'];
+$images = $_POST['images'];
+$result = mysqli_query($conn, "SELECT * FROM token WHERE  token='$token' AND user_ID='$user_ID'");
+if (!(mysqli_num_rows($result))) {
+    echo newjson(18, 'token操作错误，拒绝服务', $array);
+} else {
+    $sql = "INSERT INTO goods (name, price, shop_ID,school_ID) VALUES ('$name','$price','$shop_ID','$school_ID')";
+    $new = mysqli_query($conn, $sql);
+    $goods_id = $conn->insert_id;
+    $imageArray = explode(', ', substr($images, 1, -1));
+    foreach ($imageArray as $one_image) {
+        mysqli_query($conn, "INSERT INTO `goods_image`(`goods_ID`, `url`) VALUES ('$goods_id','$one_image')");
+    }
+    echo newjson(19, '操作成功', $array);
 }
-?>
